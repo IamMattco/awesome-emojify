6ce9f8b69efe5946c69876f59d3f90c67e549307
dev.mroz@gmail.com <dev.mroz@gmail.com>
2016-06-04 12:13:35 +0200

    source file for awesome-emojify.js

--- awesome-emojify.js
+++ awesome-emojify.js
@@ -1,15 +1,14 @@
 (function ( $ ) {
   $.fn.awesome_emojify = function( options ) {
     var settings = $.extend({
-        folder: "/images/emoji/",
-        emoticons = {
+        emoticons: {
           ':-)'     : 'emoji-1.png',
           ';-)'     : 'emoji-2.png',
           ':-D'     : 'emoji-3.png',
           ':-d'     : 'emoji-3.png',
           ':-o'     : 'emoji-4.png',
           ':-O'     : 'emoji-4.png',
-          '&lt;3'      : 'emoji-5.png',
+          '<3'      : 'emoji-5.png',
           ':-('     : 'emoji-6.png',
           ':-p'     : 'emoji-7.png',
           ':-P'     : 'emoji-7.png',
@@ -17,38 +16,34 @@
           ':-/'     : 'emoji-8.png',
           ':)'      : 'emoji-1.png',
           ';)'      : 'emoji-2.png',
-          ':D'     : 'emoji-3.png',
-          ':d'     : 'emoji-3.png',
-          ':o'     : 'emoji-4.png',
-          ':O'     : 'emoji-4.png',
-          ':('     : 'emoji-6.png',
-          ':p'     : 'emoji-7.png',
-          ':P'     : 'emoji-7.png',
-          ':/'     : 'emoji-8.png',
-          ':|'     : 'emoji-8.png',
+          ':D'      : 'emoji-3.png',
+          ':d'      : 'emoji-3.png',
+          ':o'      : 'emoji-4.png',
+          ':O'      : 'emoji-4.png',
+          ':('      : 'emoji-6.png',
+          ':p'      : 'emoji-7.png',
+          ':P'      : 'emoji-7.png',
+          ':/'      : 'emoji-8.png',
+          ':|'      : 'emoji-8.png'
         },
         width: '24px',
-        height: '20px'
+        height: '24px',
+        images_path: 'images/',
+        text: $(this).text()
     }, options );
 
-    if (window.location.hostname === 'localhost') {
-      windowUrl = "localhost:3000";
-    } else {
-      windowUrl = window.location.hostname;
-    }
-
-    url = "http://" + windowUrl + settings.folder;
+    url = settings.images_path;
     var patterns = [],
        metachars = /[[\]{}()*+?.\\|^$\-,&#\s]/g;
 
     for (var i in settings.emoticons) {
-      if (emoticons.hasOwnProperty(i)){
+      if (settings.emoticons.hasOwnProperty(i)){
         patterns.push('('+i.replace(metachars, "\\$&")+')');
       }
     }
-    return text.replace(new RegExp(patterns.join('|'),'g'), function (match) {
+    return settings.text.replace(new RegExp(patterns.join('|'),'g'), function (match) {
       return typeof settings.emoticons[match] !== 'undefined' ?
-      '<img src="'+url+emoticons[match]+'" style="width: '+ settings.width +'; height: '+ settings.height +';"/>' :
+      '<img src="'+url+settings.emoticons[match]+'" style="width: '+ settings.width +'; height: '+ settings.height +';"/>' :
       match;
     });
   };
